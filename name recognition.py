6632888b1d62268245ec5cd2edacc4db7cd15d5c
Rohan Vijay <rohanvj17@live.com>
2020-06-06 02:11:38 +0530

    Update name recognition.py

--- name recognition.py
+++ name recognition.py
@@ -1,12 +1,10 @@
-import time
-
 import speech_recognition as sr
 import smtplib
 
 message='SOMEONE CALLED OUT YOUR NAME!'
 message2='SOMEONE CALLED OUT YOUR NAME IN A SENTENCE!'
-email="sendtorohanvj@gmail.com"
-passw="Rohan@123"
+email="YOUR EMAIL ID"
+passw="YOUR PASSWORD"
 server = smtplib.SMTP("smtp.gmail.com",587)
 server.starttls()
 server.login(email,passw)
@@ -17,7 +15,7 @@ def recognize_speech_from_mic(recognizer, microphone):
     "success": a boolean indicating whether or not the API request was
                successful
     "error":   `None` if no error occured, otherwise a string containing
-               an error message if the API could not be reached orZZZZZZZZZZZZ
+               an error message if the API could not be reached or
                speech was unrecognizable
     "transcription": `None` if speech could not be transcribed,
                otherwise a string containing the transcribed text
@@ -53,7 +51,6 @@ def recognize_speech_from_mic(recognizer, microphone):
 
     return response
 
-
 if __name__ == "__main__":
     maxvalue = 1000000000
     PROMPT_LIMIT = 10000
@@ -61,12 +58,8 @@ if __name__ == "__main__":
     # create recognizer and mic instances
     recognizer = sr.Recognizer()
     microphone = sr.Microphone(chunk_size=4096)
-
-    # get a random word from the list
-    word = 'Rohan'
-    word2='rohan rohan'
-
     
+    word = 'Rohan'
 
     for i in range(maxvalue):
     
@@ -85,20 +78,19 @@ if __name__ == "__main__":
                 print("----")
             
 
-        # if there was an error, stop the game
+        # if there was an error, stop the process
         if guess["error"]:
             print("ERROR: {}".format(guess["error"]))
             continue
 
-        # show the user the transcription
         print("You said: {}".format(guess["transcription"]))
         if guess["transcription"].find("Rohan")!=-1 or guess["transcription"].find("Roman")!=-1 and len(guess["transcription"])>5:
             message2=guess["transcription"]
             server.sendmail(email,email,message2)
            # server.quit()
             print("YOUR NAME WAS CALLED IN A SENTENCE, MAIL SENT")
-        # determine if guess is correct and if any attempts remain
-        guess_is_correct = (guess["transcription"].lower() == word.lower() or guess["transcription"].lower() == word2.lower())
+        # determine if word is correct and if any attempts remain
+        guess_is_correct = (guess["transcription"].lower() == word.lower())
         user_has_more_attempts = i < maxvalue - 1
 
         if guess_is_correct:
